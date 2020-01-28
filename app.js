ecede1717c839f1010830349bbaf0420ca88b943
Ieva Vyliaudaite <ievavyl@yahoo.com>
2020-01-28 17:29:48 +0200

    saving progress

--- app.js
+++ app.js
@@ -1,33 +1,45 @@
 //BUTTONS
 
-const startsWithButton = document.querySelector('#starts-with-button');
-const searchAnyWord = document.querySelector('#search-word-button');
-const reverseIcon = document.querySelector('#reverse');
+const startsWithButton = document.querySelector('.starts-with-button');
+const searchAnyWord = document.querySelector('.search-word-button');
+const reverseButton = document.querySelector('.reverse-icon');
+document.querySelector('#p-total-count').innerHTML+=`${countries.length}`;
+
 
 const displayContainer = document.querySelector('.display-container');
 const userInput = document.querySelector('#user-input');
 
 const display = (arr) => {    
-    displayContainer.textContent ='';
+    displayContainer.innerHTML ='';
     for (const country of arr) {
         const div = document.createElement('div');
         div.setAttribute('class', 'div-country');
-        div.textContent = country;
+        div.textContent = country.toUpperCase();
         displayContainer.appendChild(div);
     }
     
 }
 display(countries);
 
+const infoAppending = () => {
+    const titles = document.querySelector('.titles');
+    const line = `<p>Countries containing <i style="color:red"> ${userInput.value} </i>  are <span style="color:lightgreen">${arr.length}</span></p>`
+    titles.innerHTML += line;
+}
+
 const searchStart = (arr) => {  
-    return arr.filter((country => country.startsWith(userInput.value)));   
+    return filteredArray = arr.filter((country => country.startsWith(userInput.value)));
+   
 }
 
 const searchAny = (arr) => {
     return arr.filter((country => country.includes(userInput.value)));
 }
 
-
+const reversing = (arr) => {
+    //const result =[...arr]
+    return arr.reverse()
+}
 
 //USER ACTIONS
 startsWithButton.addEventListener('click', () => {   
@@ -40,7 +52,7 @@ searchAnyWord.addEventListener('click', () => {
     userInput.value = null;
 })
 
-reverseIcon.addEventListener('click', () => {
-    let result = countries.reverse();
-    display(result);
+reverseButton.addEventListener('click', () => {
+
+    reversing(display(arr))
 })
