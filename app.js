62cf4806f95f446acec97679bc5a6d73edf782a1
Ieva Vyliaudaite <ievavyl@yahoo.com>
2020-01-31 15:02:14 +0200

    saving progress. working on infoAppending

--- app.js
+++ app.js
@@ -15,20 +15,11 @@ const displayContainer = document.querySelector('.display-container');
 let copy = [...countries];
 let filtered = copy;
 
-// let startClicked = false;
-// let includeClicked = false;
-
-// if (userInput.value != '') {
-//     startClicked = true;
-//     includeClicked = true;
-// }
-
 let appending = false;
 
 if (userInput.value != '') {
-    appending;
+    appending = true;
 }
-
 
 const display = (arr) => {       
     displayContainer.innerHTML ='';
@@ -39,11 +30,9 @@ const display = (arr) => {
         displayContainer.appendChild(div);
     }   
     userInput.value = '';
-    //includeClicked = false;
 }
 
-
-const infoAppending = (arr) => {
+const infoAppending = (arr) => { // input value WHERE FROM ???
     if (appending) {
         const line = `<p>Countries containing <i style="color:red"> ${userInput.value} </i>  are <span style="color:lightgreen">${arr.length}   </span></p>`;
         titles.innerHTML += line;      
@@ -65,25 +54,21 @@ const reversing = (arr) => {
     return arr.reverse()
 }
 
-
-
 //USER ACTIONS 
 searchStartButton.addEventListener('click', () => {  
     display(searchStart(copy)); 
-});
+ });
 
 searchAnyButton.addEventListener('click', () => {
-    display(searchAny(copy))
-    //includeClicked = true
-     
+    display(searchAny(copy))     
 })
 
 reverseButton.addEventListener('click', () => {
     if (userInput.value = '') {
         display(reversing(copy))
     } else {
-        display(reversing(filtered))
-    }   
+       display(reversing(filtered))
+    }    
 })
 
 searchIcon.addEventListener('click', () => {
@@ -100,3 +85,23 @@ searchIcon.addEventListener('click', () => {
 
 //STARTING
 display(copy);
+
+// document.querySelectorAll('.button-block').forEach(item => {
+//     item.addEventListener('click', (e) => {
+//         searchStartButton = display(searchStart(copy));
+//         searchAnyButton = display(searchAny(copy));
+//         if (userInput.value = '') {
+//             reverseButton = display(reversing(copy))
+//         } else {
+//             reverseButton = display(reversing(filtered))
+//         } 
+//         searchIcon = display(searchAny(copy));
+//     })
+//     console.log(e.target)
+// });
+//combine click events;
+// buttonBlock.addEventListener('click', (e) => {
+//     //alert(e.target)
+// })
+
+//create event for input to capture userInput.value to be able to check wether appending is true or not
